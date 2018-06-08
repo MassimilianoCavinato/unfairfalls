a646f503f7aef08c38a64e3b6d8d845e89aba09a
MassimilianoCavinato <massi304477@live.it>
2018-06-08 12:23:26 +0100

    server port

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var app = express();
 var server = http.Server(app);
 var io = socket.listen(server);
 
+var port = process.env.PORT || 5000;
+
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/src',express.static(__dirname + '/src'));
 app.use('/assets',express.static(__dirname + '/assets'));
 
-server.listen(8081,function(){ // Listens to port 8081
-    console.log('Listening on '+server.address().port);
+server.listen(port, function(){ // Listens to port 8081
+    console.log('Server listening on port :', port);
 });
 
 app.get('/',function(req,res){
