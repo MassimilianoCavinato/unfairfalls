7564b2cbfddb87263f25f5e67609b75d875de4c9
MassimilianoCavinato <massi304477@live.it>
2018-06-08 21:48:48 +0100

    simplified commands

--- src/game.js
+++ src/game.js
@@ -14,7 +14,7 @@ var game = new Phaser.Game(
     800,
     600,
     Phaser.CANVAS,
-    '',
+    document.getElementById('unfairfalls'),
     {
         preload: preload,
         create: create,
@@ -22,81 +22,82 @@ var game = new Phaser.Game(
     }
 );
 
-var salmon;
-var speed;
-var stamina;
-var spaceKey;
-var underControl;
+var player;
+
 
 function preload(){
-    game.load.image('salmon', 'http://localhost:5000/assets/salmon.png');
+    game.load.image('player', 'http://localhost:5000/assets/salmon.png');
 }
 
 function create(){
 
-    salmon = game.add.sprite(400, 300, 'salmon');
-    stamina = 50;
-    spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    underControl = true;
-    speed = 200;
-
-    salmon.anchor.setTo(0.5);
-    salmon.scale.setTo(0.2,0.2);
-    game.physics.arcade.enable(salmon);
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     game.stage.backgroundColor = '#ccffff';
-    salmon.body.collideWorlBounds = true;
+    createPlayer();
+
 }
 
 function update(){
-    controlSalmon();
+    controlPlayer();
 }
 
-function controlSalmon(){
 
-    if(speed > 1400){
-        speed = 1400;
-    }
-    if(speed < 200){
-        speed = 200;
-    }
-    if(spaceKey.justDown && stamina > 10){
-        speed = 400;
-    }
+// function render() {
+//     game.debug.bodyInfo(player, 32, 32);
+//     game.debug.body(player);
+// }
 
-    if(spaceKey.isDown){
+function createPlayer(){
+    player = game.add.sprite(400, 300, 'player');
+    player.anchor.setTo(0.5);
+    player.scale.setTo(0.12,0.12);
+    game.physics.arcade.enable(player);
+    player.body.collideWorlBounds = true;
+}
 
-        if(stamina > 0){
-            speed += 25;
-            stamina--;
-        }
-        else{
-            if(speed > 200){
-                speed -= 50;
-            }
-        }
-    }
-    else{
-        if(speed > 200){
-            speed -= 50;
-        }
+function controlPlayer(){
+    player.rotation = game.physics.arcade.moveToPointer(player, 0, game.input.activePointer, 280);
+    player.scale.y = player.x > game.input.x ? - Math.abs(player.scale.y) : Math.abs(player.scale.y);
 
-        if(stamina < 50){
-            stamina += 0.5;
-        }
-    }
-
-    if(Math.abs(salmon.x - game.input.x) < 40 && Math.abs(salmon.y - game.input.y) < 40){
-        game.physics.arcade.moveToPointer(salmon, 0);
-    }
-    else{
-        game.physics.arcade.moveToPointer(salmon, speed);
-        salmon.rotation = game.physics.arcade.angleToPointer(salmon);
-        if(salmon.x > game.input.x){
-            salmon.scale.y = - Math.abs(salmon.scale.y);
-        }
-        else{
-            salmon.scale.y = Math.abs(salmon.scale.y);
-        }
-    }
-    console.log(stamina, speed);
+    // if(speed > 1400){
+    //     speed = 1400;
+    // }
+    // if(speed < 200){
+    //     speed = 200;
+    // }
+    // if(spaceKey.justDown && stamina > 10){
+    //     speed = 400;
+    // }
+    //
+    // if(spaceKey.isDown){
+    //
+    //     if(stamina > 0){
+    //         speed += 25;
+    //         stamina--;
+    //     }
+    //     else{
+    //         if(speed > 200){
+    //             speed -= 50;
+    //         }
+    //     }
+    // }
+    // else{
+    //     if(speed > 200){
+    //         speed -= 50;
+    //     }
+    //
+    //     if(stamina < 50){
+    //         stamina += 0.5;
+    //     }
+    // }
+    //
+    // if(Math.abs(player.x - game.input.x) < 40 && Math.abs(player.y - game.input.y) < 40){
+    //     game.physics.arcade.moveToPointer(player, 0);
+    // }
+    // else{
+    //     game.physics.arcade.moveToPointer(player, speed);
+    //     player.rotation = game.physics.arcade.angleToPointer(player);
+    //
+    // }
+    // console.log(stamina, speed);
 }
