974d967051a2f7b5940bf2ef3a332f25cbb921ee
MassimilianoCavinato <massi304477@live.it>
2018-06-09 01:17:43 +0100

    player poly

--- src/game.js
+++ src/game.js
@@ -18,23 +18,25 @@ var game = new Phaser.Game(
     {
         preload: preload,
         create: create,
-        update: update
+        update: update,
+        render: render
     }
 );
 
 var player;
-
+var poly;
 
 function preload(){
     game.load.image('player', 'https://unfairfalls.herokuapp.com/assets/salmon.png');
+
 }
 
 function create(){
 
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.stage.backgroundColor = '#ccffff';
-    createPlayer();
+    game.physics.startSystem(Phaser.Physics.P2JS);
 
+    createPlayer();
+    // game.stage.backgroundColor = '#ccffff';
 }
 
 function update(){
@@ -42,20 +44,29 @@ function update(){
 }
 
 
-function render() {
-    game.debug.bodyInfo(player, 32, 32);
-    game.debug.body(player);
-}
-
 function createPlayer(){
+    poly = new Phaser.Polygon([ new Phaser.Point(200, 100), new Phaser.Point(350, 100), new Phaser.Point(375, 200), new Phaser.Point(150, 200) ]);
     player = game.add.sprite(400, 300, 'player');
     player.anchor.setTo(0.5);
     player.scale.setTo(0.12,0.12);
-    game.physics.arcade.enable(player);
-    player.body.collideWorlBounds = true;
+    game.physics.p2.enable([ player ], true);
+    player.body.clearShapes();
+    poly = [{
+            "shape": [ 0,40, 0,20, 10,28, 10, 33 ]
+        },
+        {
+            "shape": [ 10,33, 10,28, 35,18, 62,28, 62,33, 35,43 ]
+        },
+    ];
+    player.body.loadPolygon(null, poly);
 }
 
 function controlPlayer(){
-    player.rotation = game.physics.arcade.moveToPointer(player, 2000, game.input.activePointer, 240);
+    player.body.rotation = game.physics.arcade.moveToPointer(player, 60, game.input.activePointer, 200);
     player.scale.y = player.x > game.input.x ? - Math.abs(player.scale.y) : Math.abs(player.scale.y);
+}
+
+
+function render() {
+    game.debug.body(player);
 }
