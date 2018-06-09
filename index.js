7b6284caf56ac64c334efb3304ff5cbb675a08e9
MassimilianoCavinato <massi304477@live.it>
2018-06-10 00:54:35 +0100

    more sockets

--- index.js
+++ index.js
@@ -5,12 +5,40 @@ var socket = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socket.listen(server);
+var players = {};
+
 
 var port = process.env.PORT || 5000;
 
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/src',express.static(__dirname + '/src'));
 app.use('/assets',express.static(__dirname + '/assets'));
+
+io.on('connection', function (socket) {
+    //CONNECTION
+    players[socket.id] = {
+        id: socket.id,
+    }
+
+    // send the players object to the new player
+    socket.emit('currentPlayers', players);
+    // update all other players of the new player
+    socket.broadcast.emit('newPlayer', players[socket.id].id);
+
+    //DISCONNECTION
+    socket.on('disconnect', function () {
+        // remove this player from our players objec
+        delete players[socket.id];
+        // emit a message to all players to remove this player
+        io.emit('disconnect', socket.id);
+        console.log('user disconnected');
+    });
+
+    socket.on('playerAction', function(playerData) {
+        players[socket.id] = playerData;
+        socket.broadcast.emit('playerActionFinished', players[socket.id]);
+    });
+});
 
 server.listen(port, function(){
     console.log('Server listening on port :', port);
--- src/game.js
+++ src/game.js
@@ -1,15 +1,3 @@
-// var game = new Phaser.Game(
-//     800,
-//     600,
-//     Phaser.AUTO,
-//     document.getElementById('unfairfalls')
-// );
-// game.state.add('Game',Game);
-// game.state.start('Game');
-// var Game = {};
-// Game.init = function(){
-//     game.stage.disableVisibilityChange = true;
-// };
 var game = new Phaser.Game(
     '100%',
     '100%',
@@ -22,59 +10,154 @@ var game = new Phaser.Game(
         render: render
     }
 );
-
 var player;
-var poly;
+var otherPlayers;
+playerPolyRight = [
+    {
+        "shape": [
+            0,40,
+            0,20,
+            10,28,
+            38,22,
+            62,28,
+            62,33,
+            42,40,
+            22,38,
+            18,40,
+            10,33
+        ]
+    },
+];
+playerPolyLeft = [
+    {
+        "shape": [
+
+            62,-28,
+            62,-33,
+            42,-40,
+            22,-38,
+            18,-40,
+            10,-33,
+            0,-40,
+            0,-20,
+            10,-28,
+            38,-22,
+        ]
+    },
+];
 
 function preload(){
     game.load.image('player', 'https://unfairfalls.herokuapp.com/assets/salmon.png');
-
 }
 
 function create(){
 
-    game.physics.startSystem(Phaser.Physics.P2JS);
+    socket = io();
 
-    createPlayer();
-    game.stage.backgroundColor = '#0000A0';
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    otherPlayers = game.add.group();
+
+    socket.on('currentPlayers', function (players) {
+
+        Object.keys(players).forEach(function (playerId) {
+            playerId === socket.id ? addPlayer(players[playerId].id) : addOtherPlayer(players[playerId].id);
+        });
+    });
+
+    socket.on('newPlayer', function (playerId) {
+        console.log('NEW PLAYER', playerId);
+        addOtherPlayer(playerId);
+    });
+
+    socket.on('disconnect', function (playerId) {
+        console.log('PLAYER DISCONNECTED', playerId);
+
+        otherPlayers.children.forEach(function (otherPlayer) {
+            console.log("DISCONNECT ?", otherPlayer.id, playerId);
+            if (playerId === otherPlayer.id) {
+                otherPlayer.destroy();
+            }
+        });
+    });
+
+    socket.on('playerActionFinished', function (playerData) {
+        otherPlayers.children.forEach(function (otherPlayer){
+
+            if(otherPlayer.id === playerData.id && otherPlayer.id != player){
+
+                 // otherPlayer.x = playerData.x;
+                 // otherPlayer.y = playerData.y;
+                 otherPlayer.x = playerData.x;
+                 otherPlayer.y = playerData.y;
+                 otherPlayer.rotation = playerData.r;
+                 game.physics.p2.enable([ otherPlayer ], true);
+                 otherPlayer.body.clearShapes();
+                 otherPlayer.body.loadPolygon(null, playerPolyRight);
+                 otherPlayer.body.x = playerData.x;
+                 otherPlayer.body.y = playerData.y;
+                 otherPlayer.body.rotation = playerData.r;
+            }
+            // if (playerInfo.playerId === otherPlayer.id) {
+            //
+            //
+            // }
+
+        });
+    });
 }
 
 function update(){
-    controlPlayer();
+
+    if(typeof(player) !== 'undefined'){
+        controlPlayer();
+    }
+
 }
 
 
-function createPlayer(){
+function addPlayer(playerId){
 
     player = game.add.sprite(400, 300, 'player');
     player.anchor.setTo(0.5);
     player.scale.setTo(0.12,0.12);
-
-    //label
-    var style = { font: "120px Arial", fill: "#ffffff" };
-    var playerName = game.add.text(-300, -300, "Player Name", style);
-
-
     game.physics.p2.enable([ player ], true);
-    player.addChild(playerName);
-    //hitbox
     player.body.clearShapes();
-    poly = [{
-            "shape": [ 0,40, 0,20, 10,28, 10, 33 ]
-        },
-        {
-            "shape": [ 10,33, 10,28, 35,18, 62,28, 62,33, 35,43 ]
-        },
-    ];
-    player.body.loadPolygon(null, poly);
+    player.body.loadPolygon(null, playerPolyRight);
+    player.id = playerId;
+
+}
+
+function addOtherPlayer(playerId){
+
+    otherPlayer = game.add.sprite(400, 300, 'player');
+    otherPlayer.anchor.setTo(0.5);
+    game.physics.p2.enable([ otherPlayer ], true);
+    otherPlayer.scale.setTo(0.12,0.12);
+    otherPlayer.body.clearShapes();
+    otherPlayer.body.loadPolygon(null, playerPolyRight);
+    otherPlayer.id = playerId;
+
+    otherPlayers.add(otherPlayer);
 }
 
 function controlPlayer(){
     player.body.rotation = game.physics.arcade.moveToPointer(player, 60, game.input.activePointer, 400);
-    player.scale.y = player.x > game.input.x ? - Math.abs(player.scale.y) : Math.abs(player.scale.y);
+
+    if(game.input.x > player.x){
+        player.scale.y = Math.abs(player.scale.y);
+        player.body.clearShapes();
+        player.body.loadPolygon(null, playerPolyRight);
+    }else{
+        player.scale.y = - Math.abs(player.scale.y);
+        player.body.clearShapes();
+        player.body.loadPolygon(null, playerPolyLeft);
+    }
+
+    socket.emit('playerAction', {id: player.id, x: player.x, y: player.y, r: player.rotation});
+
 }
 
-
+//
 function render() {
-    game.debug.body(player);
+    // game.debug.body(player);
 }
