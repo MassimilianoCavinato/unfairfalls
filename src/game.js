555856d19f5b095f46b2461c177e6b92b164a2c7
MassimilianoCavinato <massi304477@live.it>
2018-06-10 23:49:15 +0100

    sockets

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 var game = new Phaser.Game(
-    '100%',
-    '100%',
+    800,
+    600,
     Phaser.CANVAS,
     document.getElementById('unfairfalls'),
     {
@@ -31,22 +31,25 @@ playerPolyRight = [
     },
 ];
 
-
 function preload(){
     game.load.image('player', 'https://unfairfalls.herokuapp.com/assets/salmon.png');
+    game.load.image('background', 'http://localhost:5000/assets/grid.png');
 }
 
 function create(){
 
     socket = io();
+    game.world.setBounds(0, 0, 6000, 6000);
+    game.add.tileSprite(0, 0, 6000, 6000, 'background');
     game.physics.startSystem(Phaser.Physics.P2JS);
     game.physics.p2.setImpactEvents(true);
+
     game.stage.disableVisibilityChange = true;
+
     playerCollisionGroup = game.physics.p2.createCollisionGroup();
     otherPlayers = game.add.physicsGroup(Phaser.Physics.P2JS);
-
     handleSockets();
-
+    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
 }
 
 function update(){
@@ -55,9 +58,8 @@ function update(){
     }
 }
 
-
 function addPlayer(playerId){
-    player = game.add.sprite(400, 300, 'player');
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
     player.anchor.setTo(0.5, 0.5);
     player.scale.setTo(0.12,0.12);
     player.oldPos = {
@@ -70,14 +72,15 @@ function addPlayer(playerId){
         speed: 0
     };
     game.physics.p2.enable([ player ], true);
-
     player.body.clearShapes();
     player.body.loadPolygon(null, playerPolyRight);
+    player.body.collideWorldBounds=true;
     player.body.mass = 10;
     player.id = playerId;
     player.timestamp = Date.now();
     player.body.setCollisionGroup(playerCollisionGroup);
     player.body.collides([playerCollisionGroup]);
+    game.camera.follow(player);
 }
 
 function addOtherPlayer(playerId){
@@ -96,23 +99,20 @@ function addOtherPlayer(playerId){
 }
 
 function controlPlayer(){
-    player.body.rotation = game.physics.arcade.moveToXY(player, game.input.activePointer.x, game.input.activePointer.y, 60, 500);
-    player.scale.y = game.input.x < player.x ? - Math.abs(player.scale.y) :  Math.abs(player.scale.y);
-    // if(game.input.x > player.x){
-    //     player.scale.y = Math.abs(player.scale.y);
-    // }else{
-    //     player.scale.y = - Math.abs(player.scale.y);
-    // }
+    var cx = game.input.activePointer.x+game.camera.x;
+    var cy = game.input.mousePointer.y+game.camera.y;
+    player.body.rotation = game.physics.arcade.moveToXY(player, cx, cy, 400);
+    player.scale.y = cx < player.x ? - Math.abs(player.scale.y) :  Math.abs(player.scale.y);
 
-    if(game.input.activePointer.x != player.oldPos.cx || game.input.activePointer.y != player.oldPos.y){
+    if(cx != player.oldPos.cx || cy != player.oldPos.cy){
         player.oldPos = {
-            cx: game.input.activePointer.x,
-            cy: game.input.activePointer.y
+            cx: cx,
+            cy: cy
         };
         socket.emit('playerAction', {
             id: player.id,
-            cx: game.input.activePointer.x,
-            cy: game.input.activePointer.y,
+            cx: cx,
+            cy: cy,
             speed:  Math.sqrt(Math.pow(player.body.velocity.x, 2) + Math.pow(player.body.velocity.y, 2))
         });
 
@@ -125,7 +125,10 @@ function controlOtherPlayer(otherPlayer, playerData){
 }
 
 function render() {
-
+    game.debug.cameraInfo(game.camera, 32, 32);
+    if(typeof player !== 'undefined'){
+        game.debug.spriteInfo(player, 500, 32);
+    }
 }
 
 function handleSockets(){
@@ -134,7 +137,6 @@ function handleSockets(){
         Object.keys(players).forEach(function (playerId, index) {
             playerId === socket.id ? addPlayer(players[playerId].id) : addOtherPlayer(players[playerId].id);
         });
-
     });
 
     socket.on('newPlayer', function (playerId) {
@@ -150,10 +152,8 @@ function handleSockets(){
     });
 
     socket.on('playerActionFinished', function (playerData) {
-
         if(playerData.id !== socket.id){
             controlOtherPlayer(otherPlayers.children[otherPlayersRef[playerData.id]], playerData);
         }
     });
-
 }
